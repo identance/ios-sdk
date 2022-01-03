01ca6c3f62313b2f5c86ac9cff97ae2819fe127b
Identance <49785688+identance@users.noreply.github.com>
2022-01-03 19:18:37 +0200

    [CI] Updated SDK to version 2.7.1

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
             targets: ["Identance"]),
     ],
     targets: [
-        .binaryTarget(name: "Identance", url: "https://download.identance.com/sdk/ios/2.7.0/Identance.xcframework.zip", checksum: "729805bc27e037d260f400482bfbf248e07ab57945455dde970b5005409c45c6")
+        .binaryTarget(name: "Identance", url: "https://download.identance.com/sdk/ios/2.7.1/Identance.xcframework.zip", checksum: "f838cc91b0a90b00f921e312a10a29221577a513aabface078d361e702808225")
     ]
 )
